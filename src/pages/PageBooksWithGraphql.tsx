cc9725f1285a0167d6eb458357193d742f29d9b7
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-19 20:11:41 +0100

    graphql with async/await

--- src/pages/PageBooksWithGraphql.tsx
+++ src/pages/PageBooksWithGraphql.tsx
@@ -12,9 +12,8 @@ export const PageBooksWithGraphql = () => {
   }
 }	
 	`;
-
-	window
-		.fetch(
+	(async () => {
+		const response = await fetch(
 			'https://graphql.contentful.com/content/v1/spaces/ksa3691cd4ua?access_token=HybRmB8-wshplsjKMC_-UWyHW9xR82Zc8UiFL6jg518',
 			{
 				method: 'POST',
@@ -23,9 +22,10 @@ export const PageBooksWithGraphql = () => {
 				},
 				body: JSON.stringify({ query }),
 			}
-		)
-		.then((response) => response.json())
-		.then((json) => console.log(json.data));
+		);
+		const _books = await response.json();
+	})();
+
 	return (
 		<div className="page pageBooksWithGraphql">
 			<p>This is the BooksWithGraphql page.</p>
