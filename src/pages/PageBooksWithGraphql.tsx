54e76f03539662f0415ff5b6a62cd177c6580cfb
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-19 21:05:51 +0100

    finished

--- src/pages/PageBooksWithGraphql.tsx
+++ src/pages/PageBooksWithGraphql.tsx
@@ -1,17 +1,24 @@
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export const PageBooksWithGraphql = () => {
+interface IBook {
+	id: string;
+	title: string;
+	description: string;
+	bookImage: {
+		url: string;
+	}
+}
 
-	const space = import.meta.env.VITE_SPACE_ID;
-	const accessToken = import.meta.env.VITE_ACCESS_TOKEN;
-	const url = `https://graphql.contentful.com/content/v1/spaces/${space}?access_token=${accessToken}`;
-
-	const query = `
+const space = import.meta.env.VITE_SPACE_ID;
+const accessToken = import.meta.env.VITE_ACCESS_TOKEN;
+const url = `https://graphql.contentful.com/content/v1/spaces/${space}?access_token=${accessToken}`;
+const query = `
 		{
 			bookCollection {
 				items {
-				title
-				description
+					title
+					description
 					bookImage {
 						url
 					}
@@ -19,36 +26,59 @@ export const PageBooksWithGraphql = () => {
 			}
 		}	
 	`;
-	(async () => {
 
-		// FETCH
-		// const response = await fetch(
-		// 	url,
-		// 	{
-		// 		method: 'POST',
-		// 		headers: {
-		// 			'Content-Type': 'application/json',
-		// 		},
-		// 		body: JSON.stringify({ query }),
-		// 	}
-		// );
-		// const _books = await response.json();
+export const PageBooksWithGraphql = () => {
+	const [books, setBooks] = useState<IBook[]>([]);
 
-		// AXIOS
-		const response = await axios({
-			url,
-			method: 'post',
-			data: {
-				query
-			}
-		});
-		const _books = response.data;
-		console.log(_books);
-	})();
+	useEffect(() => {
+		(async () => {
+			// FETCH
+			// const response = await fetch(
+			// 	url,
+			// 	{
+			// 		method: 'POST',
+			// 		headers: {
+			// 			'Content-Type': 'application/json',
+			// 		},
+			// 		body: JSON.stringify({ query }),
+			// 	}
+			// );
+			// const data = await response.json();
+			// const _books = data.data.bookCollection.items;
+
+
+			// AXIOS
+			const response = await axios({
+				url,
+				method: 'post',
+				data: {
+					query,
+				},
+			});
+			const _books = response.data.data.bookCollection.items;
+
+			setBooks(_books);
+		})();
+	}, []);
 
 	return (
 		<div className="page pageBooksWithGraphql">
-			<p>This is the BooksWithGraphql page.</p>
+			<p>There are {books.length} books:</p>
+			<div className="books">
+				{books.map((book, i) => {
+					return (
+						<div className="book" key={i}>
+							<img src={book.bookImage.url} />
+							<div className="info">
+								<div className="title">{book.title}</div>
+								<div className="description">
+									{book.description}
+								</div>
+							</div>
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,7 +1,11 @@
+import { NavLink } from 'react-router-dom';
+
 export const PageWelcome = () => {
 	return (
 		<>
-			<p>Welcome to this site.</p>
+			<p>This site shows how to fetch and display data and images from the <a target="_blank" href="https://www.contentful.com">Contentful CMS</a>.</p>
+
+			<p>Analyze the code to see how to fetch Contentful data via <NavLink to="/booksWithRest">REST</NavLink> and via <NavLink to="/booksWithGraphql">GraphQL</NavLink></p>
 		</>
 	);
 };
